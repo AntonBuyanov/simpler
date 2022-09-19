428a9add07173e8751e2a8088d215c095fb31abc
AntonBuyanov <102428399+AntonBuyanov@users.noreply.github.com>
2022-09-19 11:03:41 +0600

    feat: response, status, headers, routes, exception 404, middleware logs

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,10 +2,14 @@ class TestsController < Simpler::Controller
 
   def index
     @time = Time.now
+    # render plain: "Plain text response"
   end
 
   def create
-
+    status 201
   end
 
+  def show
+    @test = Test.find(id: params[:id])
+  end
 end
--- lib/simpler/application.rb
+++ lib/simpler/application.rb
@@ -28,13 +28,26 @@ module Simpler
 
     def call(env)
       route = @router.route_for(env)
-      controller = route.controller.new(env)
-      action = route.action
 
-      make_response(controller, action)
+      if route
+        controller = route.controller.new(env)
+        controller.request.params.merge!(route.params)
+        action = route.action
+
+        make_response(controller, action)
+      else
+        route_not_found
+      end
     end
 
     private
+
+    def route_not_found
+      [404,
+       { 'Content-Type' => 'text/plain' },
+       ["404 Not Found\n"]
+      ]
+    end
 
     def require_app
       Dir["#{Simpler.root}/app/**/*.rb"].each { |file| require file }
@@ -53,6 +66,5 @@ module Simpler
     def make_response(controller, action)
       controller.make_response(action)
     end
-
   end
 end
